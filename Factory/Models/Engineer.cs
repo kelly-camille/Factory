3bafe57576d068838508b216333d5d4092f1ff30
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-10-14 11:53:38 -0700

    add models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,16 @@
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get;}
+  }
+}
